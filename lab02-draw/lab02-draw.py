e91026aea9fce88e07f908d58cfc73444fc045e0
Emanuel Baciu <irinelsiemanuel2007@gmail.com>
2025-02-13 15:16:29 +0100

    Imported the arcade library and created the window.

---
+++ lab02-draw/lab02-draw.py
@@ -0,0 +1,15 @@
+"""
+This project was made by Emanuel Baciu and forked from github.com/dfbarrero.
+Do not repost without permission.
+"""
+
+# We import the arcade library.
+import arcade
+
+# Summoning the window.
+WIDTH = 700
+HEIGHT = 700
+arcade.open_window(WIDTH, HEIGHT, "Python paint n' color.")
+
+
+
